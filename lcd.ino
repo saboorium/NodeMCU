c7e25b3b313190b70a42bda8fb7a20635fe15580
Abdul Saboor <abdsaboor@student.iul.ac.in>
2024-06-24 20:24:00 +0530

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -4,7 +4,7 @@
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 void setup(){
-  Wire.begin(2,0);
+  Wire.begin(D4,D3);
   lcd.init();   // initializing the LCD
   lcd.backlight(); // Enable or Turn On the backlight 
   lcd.print(" Hello Makers "); // Start Printing
