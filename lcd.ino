1688cbcbc0728561041142422084a542444a7e39
Abdul Saboor <abdsaboor@student.iul.ac.in>
2024-06-24 20:25:52 +0530

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -1,7 +1,13 @@
 #include <Wire.h>
 
 #include <LiquidCrystal_I2C.h>
+/*GND pin of I2C is connected Ground pin (GND) of the NodeMCU.
 
+VCC pin of I2C is connected Vin pin of the NodeMCU. (Because we need to supply 5v to LCD)
+
+SDA pin of I2C is connected D4 of the NodeMCU.
+
+SCL pin of I2C is connected D3 pin of the NodeM*/
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
 void setup(){
   Wire.begin(D4,D3);
