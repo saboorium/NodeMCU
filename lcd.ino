11e421575d1fd381d13cdc5ae42fc0013e6559b7
Abdul Saboor <abdsaboor@student.iul.ac.in>
2024-06-24 20:21:41 +0530

    Update lcd.ino

--- lcd.ino
+++ lcd.ino
@@ -1,24 +1,14 @@
 #include <Wire.h>
+
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-void setup() {
-  lcd.begin();
-  lcd.backlight();
-  // Additional setup code
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+void setup(){
+  Wire.begin(2,0);
+  lcd.init();   // initializing the LCD
+  lcd.backlight(); // Enable or Turn On the backlight 
+  lcd.print(" Hello Makers "); // Start Printing
 }
-
-void loop() {
-  lcd.setCursor(0, 0);
-  lcd.print("Hello, World!");
-  delay(1000);
-
-  lcd.setCursor(0, 1);
-  lcd.print("THANKS");
-  delay(800);
-
-  lcd.clear();
-
-  // Additional LCD control code
+void loop(){
+  // Nothing Absolutely Nothing!
 }
