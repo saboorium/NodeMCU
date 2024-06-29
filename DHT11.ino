711ce565dd9d2eded4b69ebed47043f3a60abbc7
Abdul Saboor <abdsaboor@student.iul.ac.in>
2024-06-29 15:07:06 +0530

    DHT using dhtLIb

--- DHT11.ino
+++ DHT11.ino
@@ -1,20 +1,25 @@
-#include <dht.h>
+#include "DHT.h"
 
-dht DHT;
+DHT dht;
 
-#define DHT11_PIN 2
-
-void setup(){
+void setup()
+{
   Serial.begin(9600);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+  dht.setup(D1); 	/* D1 is used for data communication */
 }
 
 void loop()
 {
-  int chk = DHT.read11(DHT11_PIN);
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
-  delay(1000);
+  delay(dht.getMinimumSamplingPeriod()); /* Delay of amount equal to sampling period */
+  float humidity = dht.getHumidity();/* Get humidity value */
+  float temperature = dht.getTemperature();/* Get temperature value */
+  Serial.print(dht.getStatusString());/* Print status of communication */
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.println(dht.toFahrenheit(temperature), 1);/* Convert temperature to Fahrenheit units */
 }
-
