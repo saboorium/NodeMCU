77cdc7469e54e4dc6055e4eaa9ab909d69570747
Abdul Saboor <abdsaboor@student.iul.ac.in>
2024-08-02 23:19:08 +0530

    Update main.js

--- FIREBASE/website/js/main.js
+++ FIREBASE/website/js/main.js
@@ -14,9 +14,15 @@ firebase.initializeApp(firebaseConfig);
 // Reference to your Realtime Database
 const database = firebase.database();
 
-// Example: Listening to data from a specific path
+// Function to display data
+function displayData(data) {
+  const dataContainer = document.getElementById('data');
+  dataContainer.textContent = JSON.stringify(data, null, 2);
+}
+
+// Listening to data from a specific path
 const dataRef = database.ref('path/to/data');
 dataRef.on('value', (snapshot) => {
   const data = snapshot.val();
-  console.log(data);
+  displayData(data);
 });
